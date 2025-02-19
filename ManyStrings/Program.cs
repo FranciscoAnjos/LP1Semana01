5570848b06cc15f8513e8aa9a393b7bd33f17479
FranciscoAnjos <fcariaanjos@gmail.com>
2025-02-19 19:09:38 +0000

    add concatenated and interpolated strings

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -9,8 +9,16 @@ namespace ManyStrings
         private const string T3 = "He said: \"Hi, how are you?\"";
         private const string T4 = "The file path is C:\\Users\\Francisco\\Documents";
         private const string T5 = "Heart: \u2764";
-        private const string T6 = "Euro: \u20AC"; 
-        
+        private const string T6 = "Euro: \u20AC";
+
+        private static readonly string T8 = "a" + 2;
+        private const string T9 = "abc" + " x";    
+        private const int X = 5;
+        private static readonly string T10 = "abc" + X;
+        private static readonly string T11 = $"Value of x is {X}";
+        private const int Y = 10;
+        private static readonly string T12 = $"{X} plus {Y} is equal to {X + Y}";
+
         private static void Main(string[] args)
         {
             Console.WriteLine(T1);
@@ -19,6 +27,12 @@ namespace ManyStrings
             Console.WriteLine(T4);
             Console.WriteLine(T5);
             Console.WriteLine(T6);
+            
+            Console.WriteLine(T8);
+            Console.WriteLine(T9);
+            Console.WriteLine(T10);
+            Console.WriteLine(T11);
+            Console.WriteLine(T12);
         }
     }
 }
