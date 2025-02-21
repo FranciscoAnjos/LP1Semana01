e70d0f8b32482258cd71826504f1100002ca1301
FranciscoAnjos <fcariaanjos@gmail.com>
2025-02-21 14:05:51 +0000

    add number question

--- Percentagem/Program.cs
+++ Percentagem/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Percentagem
 {
     public class Program
     {
         private static void Main(string[] args)
-        {
-            Console.WriteLine("Hello LP1!");
+        {   
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+            Console.WriteLine("Please insert 4 numbers between 0 and 1");
+            string n1 = Console.ReadLine();
+            string n2 = Console.ReadLine();
+            string n3 = Console.ReadLine();
+            string n4 = Console.ReadLine();
         }
     }
 }
