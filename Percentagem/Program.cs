0060fb63a8d1411fc5655083bd264a3c6db0102f
FranciscoAnjos <fcariaanjos@gmail.com>
2025-02-21 14:30:19 +0000

    add multiplication and writeline

--- Percentagem/Program.cs
+++ Percentagem/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Globalization;
-using System.Reflection;
 
 namespace Percentagem
 {
     public class Program
     {
         private static void Main(string[] args)
-        {  int a = 100;
+        {
             CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
             Console.WriteLine("Please insert 4 numbers between 0 and 1");
             string n1 = Console.ReadLine();
@@ -21,7 +20,15 @@ namespace Percentagem
             float n4a = float.Parse(n4);
 
             float n1m = n1a * 100;
-            
+            float n2m = n2a * 100;
+            float n3m = n3a * 100;
+            float n4m = n4a * 100;
+
+            Console.WriteLine($"  {n1m}%");
+            Console.WriteLine($"  {n2m}%");
+            Console.WriteLine($"  {n3m}%");
+            Console.WriteLine($"  {n4m}%");
+
 
         }
     }
