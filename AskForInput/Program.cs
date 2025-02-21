6c6bc520186b5374e71b1c8986c8fabcfbbeee16
FranciscoAnjos <fcariaanjos@gmail.com>
2025-02-21 13:19:47 +0000

    start folder and first input

---
+++ AskForInput/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AskForInput
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Please insert a whole number");
+            string a = Console.ReadLine();
+            
+
+
+
+        }
+    }
+}
